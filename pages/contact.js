d994cb742c2c23039cdeeab9eccf28db86e1d00a
SebasGTM <se.heckmann@outlook.com>
2022-11-12 23:26:51 +0100

    test no 2

--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,7 @@ export default function ContactPage() {
         <title>Sebastian Heckmann | Contact</title>
       </Head>
       <main>
-        <h3>Welcome to my Homepage. I'm Sebastian Heckmann and here I write about everything I like, but mainly Japan and Tech</h3>
+        <h3>Welcome to my Homepage. I'm Sebastian Heckmann and here I write about everything I like, but mainly Japan and Tech !!!!!</h3>
       </main>
     </>
   );
