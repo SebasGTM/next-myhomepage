773b86a39afa41341c13e9f82ad388f0d1a02959
SebasGTM <se.heckmann@outlook.com>
2022-11-13 21:49:45 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -4,15 +4,4 @@ const config = {
   }
 }
 
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-})
-
-module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [withMDX]
-  return plugins.reduce((acc, plugin) => plugin(acc), { ...defaultConfig, ...config })
-}
+module.exports = config
