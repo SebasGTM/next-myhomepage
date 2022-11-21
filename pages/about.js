e0ae39d20063eb9b9dbd9d9ac8db8c95c16d1a33
SebasGTM <se.heckmann@outlook.com>
2022-11-21 18:50:13 +0100

    about page fix

--- pages/about.js
+++ pages/about.js
@@ -14,14 +14,15 @@ export default function AboutPage() {
         <title>Sebastian H | About</title>
       </Head>
       <main>
-        <div>
+        <div class="about-info">
+          <h3>Info</h3>
           This website was create using React/NextJS and is being hosted on AWS.
         </div>
-        <div class="contact">
-          <h3>Contact</h3>        
+        <div class="about-contact">
+          <h3>Contact</h3>
           <a class="mailto" href="mailto:sebastian.heckmann@outlook.com"><Image src={mailIcon} class="mailicon" alt="" width={40} placeholder="empty" />sebastian<wbr />.heckmann@outlook.com</a>
         </div>
-        <div class="links">
+        <div class="about-links">
           <h3>More</h3>
           <Link href={"https://www.linkedin.com/in/sebastian-heckmann/"}><Image src={linkedIcon} class="linked-icon" alt="LinkedIn" placeholder="empty" /></Link>
           <Link href={""}><Image src={gitIcon} class="linked-icon" alt="Github" placeholder="empty" /></Link>
